757555dea0bb956835fd3636aed9039d564ab69b
unknown <klishch.dmytro@gmail.com>
2022-10-04 18:28:01 +0300

    Added model and repository

---
+++ src/main/java/com/main/photoapp/models/Tag.java
@@ -0,0 +1,30 @@
+package com.main.photoapp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "tags")
+@Entity
+public class Tag {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
+    private int id;
+
+    @Column(name = "tag")
+    @Getter
+    @Setter
+    private String tag;
+
+    public Tag(int id, String tag) {
+        this.id = id;
+        this.tag = tag;
+    }
+
+    public Tag() {
+
+    }
+}
---
+++ src/main/java/com/main/photoapp/repositories/TagsRepository.java
@@ -0,0 +1,6 @@
+package com.main.photoapp.repositories;
+
+import com.main.photoapp.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagsRepository extends JpaRepository<Tag, Integer> {}
