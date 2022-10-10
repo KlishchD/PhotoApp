66e7b2687be1f301e19254a93a3246ab162fef3b
unknown <klishch.dmytro@gmail.com>
2022-10-10 17:05:36 +0300

    Made TagsController to use TagsService

--- src/main/java/com/main/photoapp/controllers/TagsController.java
+++ src/main/java/com/main/photoapp/controllers/TagsController.java
@@ -3,6 +3,7 @@ package com.main.photoapp.controllers;
 
 import com.main.photoapp.models.Tag;
 import com.main.photoapp.repositories.TagsRepository;
+import com.main.photoapp.services.TagsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,29 +11,26 @@ import org.springframework.web.bind.annotation.*;
 public class TagsController {
 
     @Autowired
-    private TagsRepository repository;
+    private TagsService service;
 
     @GetMapping("/tag/get")
     @ResponseBody
     public Tag getTagByID(@RequestParam int id) {
-        return repository.findById(id).get();
+        return service.getTagByID(id);
     }
 
     @PostMapping("/tag/add")
     public void addTag(@RequestParam String text) {
-        repository.save(new Tag(text));
+        service.addTag(text);
     }
 
     @PostMapping("tag/remove")
     public void removeTag(@RequestParam int id) {
-        repository.deleteById(id);
-
+        service.removeTag(id);
     }
 
     @PutMapping("tag/update")
     public void updateTag(@RequestParam int id, @RequestParam String text) {
-        Tag tag = repository.findById(id).get();
-        tag.setText(text);
-        repository.save(tag);
+        service.updateTag(id, text);
     }
 }
