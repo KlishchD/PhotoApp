af050e36e28027b95c60c4f6034aec1c782b1dab
unknown <klishch.dmytro@gmail.com>
2022-10-13 11:46:25 +0300

    Added exception to TagsController

--- src/main/java/com/main/photoapp/controllers/TagsController.java
+++ src/main/java/com/main/photoapp/controllers/TagsController.java
@@ -1,6 +1,8 @@
 package com.main.photoapp.controllers;
 
 
+import com.main.photoapp.exceptions.TagAlreadyExistsException;
+import com.main.photoapp.exceptions.TagNotFoundException;
 import com.main.photoapp.models.Tag;
 import com.main.photoapp.repositories.TagsRepository;
 import com.main.photoapp.services.TagsService;
@@ -15,22 +17,22 @@ public class TagsController {
 
     @GetMapping("/tag/get")
     @ResponseBody
-    public Tag getTagByID(@RequestParam int id) {
+    public Tag getTagByID(@RequestParam int id) throws TagNotFoundException {
         return service.getTagByID(id);
     }
 
     @PostMapping("/tag/add")
-    public void addTag(@RequestParam String text) {
+    public void addTag(@RequestParam String text) throws TagAlreadyExistsException {
         service.addTag(text);
     }
 
     @PostMapping("tag/remove")
-    public void removeTag(@RequestParam int id) {
+    public void removeTag(@RequestParam int id) throws TagNotFoundException {
         service.removeTag(id);
     }
 
     @PutMapping("tag/update")
-    public void updateTag(@RequestParam int id, @RequestParam String text) {
+    public void updateTag(@RequestParam int id, @RequestParam String text) throws TagNotFoundException {
         service.updateTag(id, text);
     }
 }
