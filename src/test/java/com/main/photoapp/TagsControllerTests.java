5fc19dba7824ab56622436ae0c8a2eabe49ca4bd
unknown <klishch.dmytro@gmail.com>
2022-10-21 09:56:09 +0300

    Refactored Tags tests to use random text generation util

--- src/test/java/com/main/photoapp/TagsControllerTests.java
+++ src/test/java/com/main/photoapp/TagsControllerTests.java
@@ -2,6 +2,7 @@ package com.main.photoapp;
 
 import com.main.photoapp.Utils.TagsUtils;
 import com.main.photoapp.repositories.TagsRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Random;
+
+import static com.main.photoapp.Utils.RandomTextGenerator.getRandomTagText;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -28,38 +32,46 @@ public class TagsControllerTests {
     @Autowired
     private TagsUtils tagsUtils;
 
+    private Random random;
+
+    @BeforeEach
+    public void setUp() {
+        random = new Random(0);
+        repository.deleteAll();
+    }
+
     @Test
     public void updateTag_nonExistingId_throwException() throws Exception {
-        mockMvc.perform(post("/tag/update").param("id", "-1000").param("text", "abba"))
+        mockMvc.perform(post("/tag/update").param("id", "-100").param("text", "abba"))
                 .andExpect(status().is5xxServerError())
                 .andExpect(status().reason("Tag is not found"));
     }
 
     @Test
     public void updateTag_correctInput_updatesTag() throws Exception {
-        String time = tagsUtils.getCurrentTime();
-        String id = tagsUtils.createTag(time);
-        mockMvc.perform(post("/tag/update").param("id", id).param("text", time + time)).andExpect(status().isOk());
-        assertEquals(time + time, repository.findById(Integer.valueOf(id)).orElseThrow().getText());
+        String text = getRandomTagText(random);
+        String id = tagsUtils.createTag(text);
+        mockMvc.perform(post("/tag/update").param("id", id).param("text", text)).andExpect(status().isOk());
+        assertEquals(text, repository.findById(Integer.valueOf(id)).orElseThrow().getText());
     }
 
     @Test
     public void removeTag_correctInput_removesTag() throws Exception {
-        String id = tagsUtils.createTag();
+        String id = tagsUtils.createTag(random);
         mockMvc.perform(post("/tag/remove").param("id", id)).andExpect(status().isOk());
         repository.existsById(Integer.valueOf(id));
     }
 
     @Test
     public void removeTag_nonExistingId_throwsException() throws Exception {
-        mockMvc.perform(post("/tag/remove").param("id", "-1000"))
+        mockMvc.perform(post("/tag/remove").param("id", "-100"))
                 .andExpect(status().is5xxServerError())
                 .andExpect(status().reason("Tag is not found"));
     }
 
     @Test
     public void addTag_correctInput_addsTag() throws Exception {
-        MvcResult result = mockMvc.perform(post("/tag/add").param("text", tagsUtils.getCurrentTime()))
+        MvcResult result = mockMvc.perform(post("/tag/add").param("text", getRandomTagText(random)))
                 .andExpect(status().isOk())
                 .andReturn();
         String id = result.getResponse().getContentAsString();
@@ -68,27 +80,27 @@ public class TagsControllerTests {
 
     @Test
     public void addTag_existingText_throwsException() throws Exception {
-        String time = tagsUtils.getCurrentTime();
-        tagsUtils.createTag(time);
-        mockMvc.perform(post("/tag/add").param("text", time))
+        String text = getRandomTagText(random);
+        tagsUtils.createTag(text);
+        mockMvc.perform(post("/tag/add").param("text", text))
                 .andExpect(status().is5xxServerError())
                 .andExpect(status().reason(equalTo("Tag with specified text already exists")));
     }
 
     @Test
     public void getTag_nonExistingId_throwsException() throws Exception {
-        mockMvc.perform(get("/tag/get").param("id", "-1000"))
+        mockMvc.perform(get("/tag/get").param("id", "-100"))
                 .andExpect(status().is5xxServerError())
                 .andExpect(status().reason("Tag is not found"));
     }
 
     @Test
     public void getTag_correctInput_returnsTag() throws Exception {
-        String time = tagsUtils.getCurrentTime();
-        String id = tagsUtils.createTag(time);
+        String text = getRandomTagText(random);
+        String id = tagsUtils.createTag(text);
         mockMvc.perform(get("/tag/get").param("id", id))
                 .andExpect(status().isOk())
-                .andExpect(content().string("{\"id\":" + id + ",\"text\":\"" + time + "\"}"));
+                .andExpect(content().string("{\"id\":" + id + ",\"text\":\"" + text + "\"}"));
     }
 
 
