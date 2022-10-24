e4208e83a4068450744e63ea8a21dd4986918f91
unknown <klishch.dmytro@gmail.com>
2022-10-24 15:15:03 +0300

    Added support for public/private desk functionality

--- src/test/java/com/main/photoapp/Utils/DesksUtils.java
+++ src/test/java/com/main/photoapp/Utils/DesksUtils.java
@@ -1,8 +1,9 @@
 package com.main.photoapp.Utils;
 
-import com.main.photoapp.exceptions.*;
+import com.main.photoapp.models.Desk.Desk;
 import com.main.photoapp.models.Desk.OwnersMapping.DeskOwnerMapping;
-import com.main.photoapp.services.DesksService;
+import com.main.photoapp.services.Desks.DesksOwnerService;
+import com.main.photoapp.services.Desks.DesksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,24 +18,42 @@ public class DesksUtils {
     private DesksService desksService;
 
     @Autowired
+    private DesksOwnerService desksOwnerService;
+
+    @Autowired
     private UsersUtils usersUtils;
 
-    public String createDesk(String creatorId, Random random) throws UserNotFoundException {
-        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), Integer.parseInt(creatorId));
+    public String createPrivateDesk(String creatorId, Random random) throws Exception {
+        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), Integer.parseInt(creatorId), Desk.DeskType.PRIVATE);
         return String.valueOf(id);
     }
 
-    public String createDesk(Random random) throws Exception {
+    public String createPrivateDesk(Random random) throws Exception {
         int creatorId = Integer.parseInt(usersUtils.createUser(random));
-        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), creatorId);
+        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), creatorId, Desk.DeskType.PRIVATE);
         return String.valueOf(id);
     }
 
-    public void addOwner(String deskId, String userId, DeskOwnerMapping.Permission permission, String adderId) throws DeskNotFoundException, UserNotFoundException, CanNotAddAnotherCreator, UserIsAlreadyDeskOwnerException, NotEnoughPermissionsException {
-        desksService.addOwnerToDesk(Integer.parseInt(deskId), Integer.parseInt(userId), permission, Integer.parseInt(adderId));
+    public String createPublicDesk(String creatorId, Random random) throws Exception {
+        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), Integer.parseInt(creatorId), Desk.DeskType.PUBLIC);
+        return String.valueOf(id);
     }
 
-    public String createDesk(String name, String description, String creatorId) throws UserNotFoundException {
-        return String.valueOf(desksService.addDesk(name, description, Integer.parseInt(creatorId)));
+    public String createPublicDesk(Random random) throws Exception {
+        int creatorId = Integer.parseInt(usersUtils.createUser(random));
+        int id = desksService.addDesk(getRandomName(random), getRandomDescription(random), creatorId, Desk.DeskType.PUBLIC);
+        return String.valueOf(id);
+    }
+
+    public void addOwner(String deskId, String userId, DeskOwnerMapping.Permission permission, String adderId) throws Exception {
+        desksOwnerService.addOwnerToDesk(Integer.parseInt(deskId), Integer.parseInt(userId), permission, Integer.parseInt(adderId));
+    }
+
+    public String createPublicDesk(String name, String description, String creatorId) throws Exception {
+        return String.valueOf(desksService.addDesk(name, description, Integer.parseInt(creatorId), Desk.DeskType.PUBLIC));
+    }
+
+    public String createPrivateDesk(String name, String description, String creatorId) throws Exception {
+        return String.valueOf(desksService.addDesk(name, description, Integer.parseInt(creatorId), Desk.DeskType.PRIVATE));
     }
 }
