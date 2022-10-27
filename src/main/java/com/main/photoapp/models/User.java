f0d6d6c4e6552c92ee047b0f6697db17f7965967
unknown <klishch.dmytro@gmail.com>
2022-10-27 10:34:24 +0300

    Added field and method to ease work with Spring security

--- src/main/java/com/main/photoapp/models/User.java
+++ src/main/java/com/main/photoapp/models/User.java
@@ -3,6 +3,7 @@ package com.main.photoapp.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 
@@ -15,17 +16,21 @@ public class User {
     @Getter @Setter
     private int id;
 
-    @Column(name = "nickname", nullable = false, unique = true)
+    @Column(name = "username")
     @Getter @Setter
     private String nickname;
 
-    @Column(name = "email", nullable = false, unique = true)
+    @Column(name = "email")
     @Getter @Setter
     private String email;
 
     @Column(name = "password", nullable = false)
     @Getter @Setter
     private String password;
+
+    @Column(name = "enabled", nullable = false)
+    @Getter @Setter
+    boolean enabled = true;
 
     public User(){
 
@@ -35,5 +40,12 @@ public class User {
         this.nickname = nickname;
         this.email = email;
         this.password = password;
+    }
+
+    public UserDetails getSecurityUserDetails() {
+        return org.springframework.security.core.userdetails.User
+                .withUsername(nickname)
+                .password(password)
+                .roles("USER").build();
     }
 }
