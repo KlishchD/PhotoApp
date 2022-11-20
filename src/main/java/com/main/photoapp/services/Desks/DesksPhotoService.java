641699c2f48c7bdf9686774b21c15e9f52ffd364
unknown <klishch.dmytro@gmail.com>
2022-11-20 17:13:45 +0200

    Updated DesksPhotoService

--- src/main/java/com/main/photoapp/services/Desks/DesksPhotoService.java
+++ src/main/java/com/main/photoapp/services/Desks/DesksPhotoService.java
@@ -1,17 +1,17 @@
 package com.main.photoapp.services.Desks;
 
-import com.main.photoapp.exceptions.DeskNotFoundException;
-import com.main.photoapp.exceptions.NoSuchPhotoOnDesk;
-import com.main.photoapp.exceptions.NotEnoughPermissionsException;
-import com.main.photoapp.exceptions.UserNotFoundException;
+import com.main.photoapp.exceptions.*;
 import com.main.photoapp.models.Desk.Desk;
 import com.main.photoapp.models.Desk.PhotoMapping.DeskPhotoMapping;
 import com.main.photoapp.models.Desk.PhotoMapping.DeskPhotoMappingId;
+import com.main.photoapp.models.Photo;
 import com.main.photoapp.repositories.DesksPhotoMappingRepository;
+import com.main.photoapp.services.PhotoService;
 import com.main.photoapp.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,62 +28,78 @@ public class DesksPhotoService {
     @Autowired
     private DesksService desksService;
     @Autowired
-    private DesksPhotoMappingRepository photos;
+    private DesksPhotoMappingRepository photosRepository;
+
+    @Autowired
+    private PhotoService photoService;
 
     public boolean isPhotoPartOfDesk(int deskId, int photoId) {
-        return photos.existsById(new DeskPhotoMappingId(deskId, photoId));
+        return photosRepository.existsById(new DeskPhotoMappingId(deskId, photoId));
     }
 
-    public void addPhotoToTable(int deskId, int photoId, int userId) throws DeskNotFoundException, NotEnoughPermissionsException, UserNotFoundException {
+    public void addPhotoToTable(int deskId, int photoId, int userId) throws DeskNotFoundException, NotEnoughPermissionsException, UserNotFoundException, PhotoNotFoundException {
         if (usersService.userNotExists(userId)) throw new UserNotFoundException(userId);
         if (desksService.deskNotExists(deskId)) throw new DeskNotFoundException(deskId);
-        //TODO: Add photo existence check
+        if (photoService.photoNotExists(photoId)) throw new PhotoNotFoundException(photoId);
         if (!desksOwnerService.getDeskOwnerPermission(deskId, userId).canAddPhoto())
             throw new NotEnoughPermissionsException(userId);
-        photos.save(new DeskPhotoMapping(deskId, photoId));
+        photosRepository.save(new DeskPhotoMapping(deskId, photoId));
     }
 
     @Transactional
-    public void removePhotoFromTable(int deskId, int photoId, int userId) throws DeskNotFoundException, NotEnoughPermissionsException, NoSuchPhotoOnDesk, UserNotFoundException {
+    public void removePhotoFromTable(int deskId, int photoId, int userId) throws DeskNotFoundException, NotEnoughPermissionsException, NoSuchPhotoOnDesk, UserNotFoundException, PhotoNotFoundException {
         if (usersService.userNotExists(userId)) throw new UserNotFoundException(userId);
         if (desksService.deskNotExists(deskId)) throw new DeskNotFoundException(deskId);
         if (photoIsNotPartOfDesk(deskId, photoId)) throw new NoSuchPhotoOnDesk(deskId, photoId);
-        //TODO: Add photo existence check
+        if (photoService.photoNotExists(photoId)) throw new PhotoNotFoundException(photoId);
         if (!desksOwnerService.getDeskOwnerPermission(deskId, userId).canRemovePhoto())
             throw new NotEnoughPermissionsException(userId);
-        photos.deleteByDeskIdAndPhotoId(deskId, photoId);
+        photosRepository.deleteByDeskIdAndPhotoId(deskId, photoId);
     }
 
 
-    public Page<Integer> getPhotosFromDesk(int deskId, int userId, int page, int pageSize) throws DeskNotFoundException, NotEnoughPermissionsException, UserNotFoundException {
+    public Page<Photo> getPhotosFromDesk(int deskId, int userId, int page, int pageSize) throws DeskNotFoundException, NotEnoughPermissionsException, UserNotFoundException {
         if (usersService.userNotExists(userId)) throw new UserNotFoundException(userId);
         if (desksService.deskNotExists(deskId)) throw new DeskNotFoundException(deskId);
         if (!desksOwnerService.canUserAccessDesk(deskId, userId)) throw new NotEnoughPermissionsException(userId);
-        return photos.findAllByDeskId(deskId, Pageable.ofSize(pageSize).withPage(page)).map(DeskPhotoMapping::getPhotoId);
+        return photosRepository.findAllByDeskId(deskId, Pageable.ofSize(pageSize).withPage(page))
+                .map(DeskPhotoMapping::getPhotoId)
+                .map(this::getPhotoById);
     }
 
     public int getPageNumber(int deskId, int size) {
-        int count = photos.countDistinctByDeskId(deskId);
+        int count = photosRepository.countDistinctByDeskId(deskId);
         return  count / size + (count % size == 0 ? 0 : 1);
     }
 
-    public Map<Integer, Integer> getFirstPhotoForDesks(List<Desk> desksIds) {
+    public Map<Integer, Photo> getFirstPhotoForDesks(List<Desk> desksIds) {
         return getFirstPhotoForDesksByIds(desksIds.stream().map(Desk::getId).toList());
     }
 
-    public Map<Integer, Integer> getFirstPhotoForDesksByIds(List<Integer> desksIds) {
-        Map<Integer, Integer> photos = new HashMap<>();
+    public Map<Integer, Photo> getFirstPhotoForDesksByIds(List<Integer> desksIds) {
+        Map<Integer, Photo> photos = new HashMap<>();
         for (int deskId: desksIds) {
             photos.put(deskId, getFirstPhotoForDesk(deskId));
         }
         return photos;
     }
 
-    public Integer getFirstPhotoForDesk(int deskId) {
-        return photos.findFirstByDeskId(deskId).map(DeskPhotoMapping::getPhotoId).orElse(null);
+    public Photo getFirstPhotoForDesk(int deskId) {
+        return photosRepository.findFirstByDeskId(deskId)
+                .map(DeskPhotoMapping::getPhotoId)
+                .map(this::getPhotoById)
+                .orElse(null);
+    }
+
+    private Photo getPhotoById(int photoId) {
+        try {
+            return photoService.getPhotoById(photoId);
+        } catch (Exception e){
+            return null;
+        }
     }
 
     private boolean photoIsNotPartOfDesk(int deskId, int photoId) {
-        return !photos.existsById(new DeskPhotoMappingId(deskId, photoId));
+        return !photosRepository.existsById(new DeskPhotoMappingId(deskId, photoId));
     }
 }
