9b61cb6c9292d3d0b6f39b05f85e2a1b2ad1e288
unknown <klishch.dmytro@gmail.com>
2022-11-20 17:08:53 +0200

    Updated UsernameChecker

--- src/main/java/com/main/photoapp/utils/UsernameChecker.java
+++ src/main/java/com/main/photoapp/utils/UsernameChecker.java
@@ -2,15 +2,15 @@ package com.main.photoapp.utils;
 
 public class UsernameChecker {
 
-    public static final int NICKNAME_MINIMAL_SIZE = 5;
-    public static final int NICKNAME_MAXIMAL_SIZE = 25;
+    public static final int USERNAME_MINIMAL_SIZE = 5;
+    public static final int USERNAME_MAXIMAL_SIZE = 25;
 
-    public static final String NICKNAME_PATTERN = "([a-z]|[A-Z]|[0-9])*";
+    public static final String USERNAME_PATTERN = "([a-z]|[A-Z]|[0-9])*";
 
     public static boolean isUsernameCorrect(String username) {
-        if (username.length() < NICKNAME_MINIMAL_SIZE) return false;
-        if (username.length() > NICKNAME_MAXIMAL_SIZE) return false;
-        return username.matches(NICKNAME_PATTERN);
+        if (username.length() < USERNAME_MINIMAL_SIZE) return false;
+        if (username.length() > USERNAME_MAXIMAL_SIZE) return false;
+        return username.matches(USERNAME_PATTERN);
     }
 
 }
